887e76292bc725a52c19b201f76115d633ce9507
eiken0525 <eiken0525@gmail.com>
2024-07-30 00:03:48 +0800

    2024/7/30 0:03 Next time add graphs for exer 2.1

--- py/inv.py
+++ py/inv.py
@@ -6,4 +6,4 @@ def inv(x, y):
 def vert(x, y):
     return x
 
-plot_tool.plot_implicit_functions([inv, vert], "The graph of $f(x)=\dfrac{1}{x}$", [0, 0], ["$f(x)=\dfrac{1}{x}$", "x=0"], (-6, 6), (-6, 6), "reciprocal_of_x", 14, 10000)
+plot_tool.plot_implicit_functions([inv, vert], "", [0, 0], ["$f(x)=\dfrac{1}{x}$", "x=0"], (-6, 6), (-6, 6), "limit_of_reci", 28, 10000)
